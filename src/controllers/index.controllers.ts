4552e0d69f996522f0fa7254baccc9aa1470c8d7
unknown <santiposse2000@gmail.com>
2022-10-15 23:18:14 -0300

    little changes

--- src/controllers/index.controllers.ts
+++ src/controllers/index.controllers.ts
@@ -3,5 +3,5 @@ import {Request, Response} from 'express'
 import { pool } from '../database'
 
 export const getUsers = (req: Request, res: Response)=>{
-    res.send("users")
+    res.send("usuarios")
 }
